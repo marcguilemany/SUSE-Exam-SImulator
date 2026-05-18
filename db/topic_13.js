5e77bfe11a0b952b3258910112ac101fbb00b027
MARC FERNANDEZ GUILEMANY <marc.guilemany@gmail.com>
2026-05-18 10:56:47 +0200

    Fix formatting in topic_13.js

--- db/topic_13.js
+++ db/topic_13.js
@@ -155,8 +155,6 @@ window.loadTopic("13 Storage Management Fundamentals", [
             {text: "mount --status", correct: false}
         ], 
         rationale: "`blkid` specifically identifies block device properties (UUID, Labels, Filesystem format), whereas `df` only interacts with currently mounted filesystems.<br><br><a href='https://documentation.suse.com/sles/15-SP5/html/SLES-all/cha-filesystems.html' target='_blank'>📚 SUSE Docs: Block Device Identification</a>" 
-    }
-    
-
+    },
 
 ]);
